ecf064332511ac1b3455823a88033d99df2d5b66
TheGreatGreeble <73497579+TheGreatGreeble@users.noreply.github.com>
2024-11-24 22:45:21 -0800

    added stuff to it donnne yes!
    YES!

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,15 +1,22 @@
 extends CharacterBody2D
 class_name EvansSuperSpecialCharacterBody2D
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+@export var flip_sprite = false
+var sprite_node: Sprite2D
 
 # runs once this node has loaded into the scene
 func _ready() -> void:
+	sprite_node = get_node("MySprite")
 	pass
 
 # runs once every frame
 func _process(delta: float) -> void:
+	
+	if (flip_sprite):
+		sprite_node.flip_v
+		flip_sprite = false
 	
 	pass
 
@@ -21,13 +28,14 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	
